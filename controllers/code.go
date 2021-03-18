2a00b327abea86d83cd33e331049a67e3f39613a
ts <youyin.gu@aisuperstar.com>
2021-03-18 15:05:27 +0800

    code返回码封装

---
+++ controllers/code.go
@@ -0,0 +1,70 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+const (
+	CodeSuccess ResCode = 1000 + iota
+	CodeInvalidParam
+	CodeUserExist
+	CodeUserNotExist
+	CodeInvalidPassword
+	CodeServerBusy
+)
+
+var codeMsgMap = map[ResCode]string{
+	CodeSuccess:         "success",
+	CodeInvalidParam:    "请求参数错误",
+	CodeUserExist:       "用户名已存在",
+	CodeUserNotExist:    "用户名不存在",
+	CodeInvalidPassword: "用户名或密码错误",
+	CodeServerBusy:      "服务繁忙",
+}
+
+type ResCode int64
+
+func (c ResCode) Msg() string {
+	msg, ok := codeMsgMap[c]
+	if !ok {
+		msg = codeMsgMap[CodeServerBusy]
+	}
+	return msg
+}
+
+type ResponseData struct {
+	Code ResCode     `json:"code"`
+	Msg  interface{} `json:"msg"`
+	Data interface{} `json:"data"`
+}
+
+//返回正常
+func ResponseSuccess(c *gin.Context, data interface{}) {
+	rd := &ResponseData{
+		Code: CodeSuccess,
+		Msg:  CodeSuccess.Msg(),
+		Data: data,
+	}
+	c.JSON(http.StatusOK, rd)
+}
+
+//自定义错误
+func ResponseWithMsg(c *gin.Context, code ResCode, msg interface{}) {
+	rd := &ResponseData{
+		Code: code,
+		Msg:  msg,
+		Data: nil,
+	}
+	c.JSON(http.StatusOK, rd)
+}
+
+//返回正常
+func ResponseError(c *gin.Context, code ResCode) {
+	rd := &ResponseData{
+		Code: code,
+		Msg:  code.Msg(),
+		Data: nil,
+	}
+	c.JSON(http.StatusOK, rd)
+}
--- controllers/user.go
+++ controllers/user.go
@@ -3,11 +3,9 @@ package controllers
 import (
 	"bluebell/api"
 	"bluebell/models"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"go.uber.org/zap"
-	"net/http"
 )
 
 //用户注册方法
@@ -15,38 +13,43 @@ func UserRegister(c *gin.Context) {
 	p := models.ParamsUser{}
 	//1.参数校验 validator库
 	if err := c.ShouldBindJSON(&p); err != nil {
-		zap.L().Error("绑定错误:", zap.Error(err))
+		zap.L().Error("参数错误:", zap.Error(err))
 		//判断err是不是validator中的类型
 		errs, ok := err.(validator.ValidationErrors)
 		if !ok {
 			// 非validator.ValidationErrors类型错误直接返回
-			c.JSON(http.StatusOK, gin.H{
-				"code": 403,
-				"msg":  err.Error(),
-			})
+			//c.JSON(http.StatusOK, gin.H{
+			//	"code": 403,
+			//	"msg":  err.Error(),
+			//})
+			ResponseWithMsg(c, CodeInvalidParam, err.Error())
 			return
 		}
-		c.JSON(http.StatusOK, gin.H{
-			"msg":  "出错",
-			"data": removeTopStruct(errs.Translate(trans)), //翻译
-		})
+		//c.JSON(http.StatusOK, gin.H{
+		//	"msg":  "出错",
+		//	"data": removeTopStruct(errs.Translate(trans)), //翻译
+		//})
+		ResponseWithMsg(c, CodeInvalidParam, removeTopStruct(errs.Translate(trans)))
 		return
 	}
 
 	//2.用户注册接口
 	if err := api.Register(&p); err != nil {
 		zap.L().Error("用户已存在：", zap.Error(err))
-		c.JSON(http.StatusOK, gin.H{
-			"msg": "用户已存在！！！",
-		})
+		//c.JSON(http.StatusOK, gin.H{
+		//	"msg": "用户已存在！！！",
+		//})
+		ResponseError(c, CodeUserExist)
 		return
 	}
 
 	//3.返回响应
-	c.JSON(http.StatusOK, gin.H{
-		"msg":  "success",
-		"data": p,
-	})
+	//c.JSON(http.StatusOK, gin.H{
+	//	"msg":  "success",
+	//	"data": p,
+	//})
+
+	ResponseSuccess(c, "注册成功！！！")
 
 }
 
@@ -59,16 +62,18 @@ func UserLogin(c *gin.Context) {
 		errs, ok := err.(validator.ValidationErrors)
 		if !ok {
 			//不是validator中的错误类型
-			c.JSON(http.StatusOK, gin.H{
-				"code": http.StatusOK,
-				"msg":  err.Error(),
-			})
+			//c.JSON(http.StatusOK, gin.H{
+			//	"code": http.StatusOK,
+			//	"msg":  err.Error(),
+			//})
+			ResponseError(c, CodeInvalidParam)
 			return
 		}
-		c.JSON(http.StatusOK, gin.H{
-			"code": 403,
-			"msg":  removeTopStruct(errs.Translate(trans)), //翻译validator中的错误
-		})
+		//c.JSON(http.StatusOK, gin.H{
+		//	"code": 403,
+		//	"msg":  removeTopStruct(errs.Translate(trans)), //翻译validator中的错误
+		//})
+		ResponseWithMsg(c, CodeInvalidParam, removeTopStruct(errs.Translate(trans)))
 		return
 	}
 	//2.用户登陆接口
@@ -77,19 +82,23 @@ func UserLogin(c *gin.Context) {
 		return
 	}
 	if exist == false {
-		c.JSON(403, gin.H{
-			"code": 403,
-			"msg":  "error",
-			"data": fmt.Sprintf("%s用户名或密码错误！！！", p.Username),
-		})
+		//c.JSON(403, gin.H{
+		//	"code": 403,
+		//	"msg":  "error",
+		//	"data": fmt.Sprintf("%s用户名或密码错误！！！", p.Username),
+		//})
+		ResponseError(c, CodeInvalidPassword)
 		return
 	}
+
 	//3.返回响应
 
-	c.JSON(http.StatusOK, gin.H{
-		"code": http.StatusOK,
-		"msg":  "success",
-		"data": "登陆成功！！！",
-	})
+	//c.JSON(http.StatusOK, gin.H{
+	//	"code": http.StatusOK,
+	//	"msg":  "success",
+	//	"data": "登陆成功！！！",
+	//})
+
+	ResponseSuccess(c, "登陆成功！！！")
 
 }
